c248d089ebc9b08088a96b6f19f303bb0464bbfb
Marcaus-Cruz <marcauscruz@gmail.com>
2025-03-28 14:46:31 -0700

    Cleanup. Create basic header

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header">
+        <Title text="Gateway" />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import Title from './Title.vue';
+</script>
+
+<style scoped>
+    .header {
+        position: fixed;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100vw;
+        height: 10vh;
+        padding: 1em;
+
+        top: 0;
+        left: 0;
+
+        color: white;
+        background-color: #7fb57f;
+    }
+</style>
