ef8fbdd0d6af3328f3c9b5797f48b556e053f2be
Marcaus.Cruz <marcauscruz@gmail.com>
2025-04-16 15:56:51 -0700

    WIP Home View

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,52 @@
 <!-- v-for="({ text, mediaUrl, mediaType, style = {} }, index) in thumbnails" -->
 <template>
-    <section>
+    <div class="view">
         <span>HOME</span>
-        <div class="cards">
-            <ThumbnailCard
-                v-for="(thumbnail, index) in thumbnails"
-                :key="index"
-                :style="thumbnail.style"
-                :mediaType="thumbnail.mediaType"
-                :mediaUrl="thumbnail.mediaUrl"
-                class="thumbnail"
-            >
-                {{ thumbnail.text }}
-            </ThumbnailCard>
-        </div>
-    </section>
+        <section class="hero">
+            <TextImageContainer
+                :text="textImage.text"
+                :mediaUrl="textImage.mediaUrl"
+                :alt="textImage.alt"
+                :type="textImage.mediaType"
+            ></TextImageContainer>
+        </section>
+        <section class="thumbnails">
+            <div class="cards">
+                <ThumbnailCard
+                    v-for="(thumbnail, index) in thumbnails"
+                    :key="index"
+                    :style="thumbnail.style"
+                    :mediaType="thumbnail.mediaType"
+                    :mediaUrl="thumbnail.mediaUrl"
+                    class="thumbnail"
+                >
+                    {{ thumbnail.text }}
+                </ThumbnailCard>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script setup>
     import { gsap } from 'gsap';
     import { onMounted, ref } from 'vue';
     import ThumbnailCard from '../components/ThumbnailCard.vue';
+    import TextImageContainer from '../components/TextImageContainer.vue';
+
+    const textImage = Object.freeze({
+        text: [
+            "Hello I'm Marcaus Cruz",
+            "I'm a Software Engineer based in the Portland Metro Area.",
+            'Thanks for visiting my site',
+        ],
+        mediaUrl: '/src/assets/img/thumb-aboutMe.jpg',
+        mediaType: 'image',
+        alt: 'Marcaus Cruz posing in front of the Eiffel Tower',
+    });
 
     const randomAnimStyles = () => ({
         animationDelay: `${Math.random() * 2}s`,
-        animationDuration: `${3 + Math.random()}s`,
+        animationDuration: `${5 + Math.random()}s`,
         animationTimingFunction: 'ease-in-out',
         animationIterationCount: 'infinite',
         animationName: 'rotateY',
@@ -53,10 +75,8 @@
 </script>
 
 <style scoped lang="scss">
-    section {
-        display: flex;
-        flex-direction: column;
-        background-color: var(--color-accent);
+    .view {
+        background-color: var(--color-background);
         margin: auto var(--header-margin-x);
 
         span {
@@ -64,22 +84,31 @@
             font-size: 3vmin;
         }
 
-        .cards {
-            position: relative;
+        section.hero {
+            margin: 5vmin auto;
+        }
+
+        section.thumbnails {
             display: flex;
+            flex-direction: column;
 
-            .thumbnail {
-                flex: 1 1 33%;
-                height: 30vh;
-                margin: 3vmin;
-            }
+            .cards {
+                position: relative;
+                display: flex;
 
-            &::after {
-                @include absolute-center($top: unset);
-                content: 'logos from https://icons8.com/';
-                font-size: 2vmin;
-                text-align: center;
-                opacity: 0.5;
+                .thumbnail {
+                    flex: 1 1 33%;
+                    height: 30vh;
+                    margin: 3vmin;
+                }
+
+                &::after {
+                    @include absolute-center($top: unset);
+                    // content: 'logos from https://icons8.com/';
+                    font-size: 2vmin;
+                    text-align: center;
+                    opacity: 0.5;
+                }
             }
         }
     }
