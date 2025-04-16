da7cd1832b7eb6daa8ede1da8e9e8929ccb5f79b
Marcaus.Cruz <marcauscruz@gmail.com>
2025-04-16 15:54:37 -0700

    Remove unused components

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,19 +1,14 @@
 <template>
     <section>
-        <ImageWithText :text="'Experience'" />
         <div class="experience-container">
             <p>This is the Experience page content.</p>
         </div>
     </section>
 </template>
 
-<script setup>
-    import ImageWithText from '@/components/ImageWithText.vue';
-</script>
+<script setup></script>
 
 <style scoped lang="scss">
     .experience-container {
-        padding: 20px;
-        margin-top: 20px;
     }
 </style>
