7491b94d7a67f11d69e7d8829e6f46ca45a59ff1
Marcaus.Cruz <marcauscruz@gmail.com>
2025-04-24 14:36:59 -0700

    Style Not Found View

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,8 +1,8 @@
 <template>
-    <section class="">
-        <div class="">!!!</div>
-        <div class="">404 Not Found</div>
-        <div class="">This page does not exist</div>
+    <section class="view not-found">
+        <div class="not-found-text warning">! ! !</div>
+        <div class="not-found-text title">404 Not Found</div>
+        <div class="not-found-text subtitle">This page does not exist</div>
         <RouterLink to="/" class="">Go Back</RouterLink>
     </section>
 </template>
@@ -10,3 +10,27 @@
 <script setup>
     import { RouterLink } from 'vue-router';
 </script>
+
+<style lang="scss">
+    .view.not-found {
+        @include flex-container(column, nowrap, center, center);
+        margin: 10%;
+
+        .not-found-text {
+            font-family: 'Montserrat Bold', sans-serif;
+
+            &.warning {
+                color: red;
+                font-size: 10vmin;
+            }
+
+            &:not(:first-child) {
+                margin: 1em auto;
+            }
+
+            &.subtitle {
+                font-family: 'Montserrat Regular', sans-serif;
+            }
+        }
+    }
+</style>
