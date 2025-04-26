7624985476fa8657005afdb86e30b9e031c58496
Marcaus.Cruz <marcauscruz@gmail.com>
2025-04-26 13:48:09 -0700

    Update deploy again...

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,27 @@
 # Abort on errors
 set -e
 
-# Build your app (this must happen first!)
+# Build your app
 npm run build-only
 
 # Go into the dist folder
 cd dist
+
+# Clean any previous git history (very important!)
+rm -rf .git
 
 # Add CNAME for custom domain
 echo 'marcauscruz.com' > CNAME
 
 # Initialize a new Git repository
 git init
+git checkout -b master
 git add -A
 git commit -m 'deploy'
 
-# Add GitHub remote and push to gh-pages
+# Add GitHub remote and push to gh-pages branch
 git remote add origin git@github.com:Marcaus-Cruz/gateway.git
 git push -f origin master:gh-pages
 
-# Go back to previous directory
+# Go back
 cd -
