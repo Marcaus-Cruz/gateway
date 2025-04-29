11393b57c17ab8edff930d62241477224ed2c347
Marcaus.Cruz <marcauscruz@gmail.com>
2025-04-29 12:38:49 -0700

    Init Carousel component

---
+++ src/components/Carousel.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="carousel-container">
+        <button class="btn expand-btn" @click="isExpanded = !isExpanded">{{ expandText }}</button>
+        <div v-show="isExpanded" class="carousel" :class="{ expanded: isExpanded }">
+            <button class="btn prev-btn" @click="prevImage">Prev</button>
+            <div class="carousel-content">
+                <TextImageContainer
+                    v-for="(carouselItem, index) in carouselItems"
+                    :key="index"
+                    :mediaUrl="carouselItem.mediaUrl"
+                    :text="carouselItem.text"
+                />
+            </div>
+            <button class="btn next-btn" @click="prevImage">Next</button>
+        </div>
+    </section>
+</template>
+
+<script setup>
+    import { ref, onMounted, computed } from 'vue';
+    import TextImageContainer from '@/components/TextImageContainer.vue';
+
+    const isExpanded = ref(false);
+
+    defineProps({
+        carouselItems: {
+            type: Array,
+            default: [],
+        },
+        expandText: {
+            type: String,
+            default: 'See More',
+        },
+    });
+</script>
+
+<style lang="scss"></style>
