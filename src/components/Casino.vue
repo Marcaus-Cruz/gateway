ca89aaeb5c42c9d745d5f78636e61a82e6da6f40
Marcaus.Cruz <marcauscruz@gmail.com>
2025-05-21 11:42:26 -0700

    Resize casino iframe

--- src/components/Casino.vue
+++ src/components/Casino.vue
@@ -2,6 +2,18 @@
     <iframe
         title="Casino"
         src="https://marcaus-cruz.github.io/le-casino/"
-        style="width: 100%; height: 100%; border: none"
+        class="iframe-casino"
     ></iframe>
 </template>
+
+<style lang="scss">
+    .iframe-casino {
+        width: 100vw;
+        height: calc(100vh - var(--header-height));
+        border: none;
+
+        @media (orientation: portrait) {
+            height: calc(100vh - var(--header-height) - 2.5em);
+        }
+    }
+</style>
